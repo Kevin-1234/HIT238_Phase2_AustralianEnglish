bac2329e746f837eba120004287e66a6a6cb0547
KevinLynn <60030989+Kevin-1234@users.noreply.github.com>
2020-10-05 16:22:12 +0930

    add image to each notebook button

--- screens/noteBook.js
+++ screens/noteBook.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, TouchableHighlight, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableHighlight, ImageBackground,TouchableOpacity, Image } from 'react-native';
 import {globalStyles} from '../styles/global';
 
 export default function Notebook({navigation}) {
@@ -16,12 +16,54 @@ export default function Notebook({navigation}) {
 
     return (
 
-        <View>
-            <Button onPress={slangsButton} title="Slang Notes"/>
-            <Button onPress={phrasesButton} title="Phrase Notes"/>
-            <Button onPress={topicsButton} title="Topic Notes"/>
+        <View style={styles.container}>
+            <TouchableOpacity onPress={slangsButton}>
+                <ImageBackground  source={require('../assets/images/notebookbutton.jpg')} style={styles.buttons} imageStyle={ styles.backgroundImage}>
+                    <Text style={styles.cardTitle}>Slang Notes</Text>
+                </ImageBackground>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={phrasesButton}>
+                <ImageBackground source={require('../assets/images/notebookbutton.jpg')} style={styles.buttons} imageStyle={ styles.backgroundImage}>
+                <Text style={styles.cardTitle}>Phrase Notes</Text>
+                </ImageBackground>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={topicsButton}>
+                <ImageBackground source={require('../assets/images/notebookbutton.jpg')} style={styles.buttons} imageStyle={ styles.backgroundImage}>
+                <Text style={styles.cardTitle}>Topic Notes</Text>
+                </ImageBackground>
+            </TouchableOpacity>       
         </View>
 
     );
 
 }
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop: '15%',
+        flexDirection: 'row',
+        justifyContent: 'flex-start', 
+        flexWrap:'wrap',
+    },
+    buttons:{
+        marginTop: '15%',
+        marginLeft: '23%',  
+        height: 150,
+        width: 110,
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',   
+        flexDirection: 'row'
+     
+    },
+    cardTitle:{
+        fontSize: 26,
+        marginTop:'26%',
+        marginLeft: '18%',
+        
+        color: '#f0f8ff'
+
+    }
+   
+
+   });
